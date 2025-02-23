77b3abaf8bdd30fe9d83717d2bbba8b94e97db7d
celetrawaza <198874505+celetrawaza@users.noreply.github.com>
2025-02-23 17:54:58 +0700

    Basic frontend

---
+++ services/frontend/src/Meetings.jsx
@@ -0,0 +1,14 @@
+import './Meetings.css'
+import Bar from "./components/menu/Bar.jsx"
+import Grid from "./components/cards/Grid.jsx"
+
+function App() {
+  return (
+    <>
+      <Bar />
+      <Grid />
+    </>
+  )
+}
+
+export default App
---
+++ services/frontend/src/components/menu/Tab.jsx
@@ -0,0 +1,9 @@
+import "./Menu.css"
+
+export default function Tab({title}) {
+    return (
+        <div className="tab">
+            {title}
+        </div>
+    )
+}
