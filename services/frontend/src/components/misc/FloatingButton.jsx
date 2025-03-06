2dadf27e1120927b40a2ead1b391f82082cd2935
celetrawaza <198874505+celetrawaza@users.noreply.github.com>
2025-03-06 19:47:07 +0700

    Event creation in modal window

--- services/frontend/src/components/misc/FloatingButton.jsx
+++ services/frontend/src/components/misc/FloatingButton.jsx
@@ -1,12 +1,15 @@
 import "./FloatingButton.css"
 import { useNavigate } from "react-router-dom";
 
-export default function FloatingButton({text, target}) {
+export default function FloatingButton({text, onClick, target}) {
     const navigate = useNavigate();
     const redirect = () => {
         navigate(target);
     }
     return (
-        <input className="floatingButton" type="button" value={text} onClick={redirect} />
+        <input className="floatingButton" type="button" value={text} onClick={()=>{
+            target && redirect();
+            onClick && onClick();
+        }} />
     );
 }
--- services/frontend/src/components/tabs/Events.jsx
+++ services/frontend/src/components/tabs/Events.jsx
@@ -3,32 +3,37 @@ import axios from "axios";
 import Grid from "../cards/Grid";
 import FloatingButton from "../misc/FloatingButton";
 import { useNavigate } from "react-router-dom";
+import { createPortal } from "react-dom";
+import ModalWindow from "../misc/ModalWindow";
+import CreateEvent from "../event/CreateEvent";
 
 export default function Events() {
     const [events, setEvents] = useState([]);
     const [subscriptions, setSubscriptions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [showCreateEvent, setShowCreateEvent] = useState(false);
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get("/api/v1/auth/refresh");
-                const responseEvents = await axios.get("/api/v1/events");
-                const responseSubscriptions = await axios.get("api/v1/registrations");
-                setEvents(responseEvents.data);
-                setSubscriptions(responseSubscriptions.data);
-                // console.log(response.data);
-            } catch (err) {
-                setError("Can't receive events");
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
-        };
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            await axios.get("/api/v1/auth/refresh");
+            const responseEvents = await axios.get("/api/v1/events");
+            const responseSubscriptions = await axios.get("api/v1/registrations/my");
+            setEvents(responseEvents.data);
+            setSubscriptions(responseSubscriptions.data);
+            // console.log(response.data);
+        } catch (err) {
+            setError("Can't receive events");
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    useEffect(() => {
         fetchData();
     }, []);
 
@@ -38,7 +43,12 @@ export default function Events() {
     return (
         <>
             <Grid cards={events} subscriptions={subscriptions} />
-            <FloatingButton text="Добавить мероприятие" target={"/events"} />
+            <FloatingButton text="Добавить мероприятие" onClick={()=>setShowCreateEvent(true)} />
+            {showCreateEvent && createPortal(
+                <ModalWindow onClose={()=>{setShowCreateEvent(false);fetchData()}}>
+                    <CreateEvent />
+                </ModalWindow>, document.body
+            )}
         </>
     );
 }
